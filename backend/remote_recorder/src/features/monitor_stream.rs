623e3d47387d9848843dc22ceb22776340c6f797
defoltusrnm <defoltusrnm@gmail.com>
2025-05-13 17:44:20 +0200

    shieeet

--- backend/remote_recorder/src/features/monitor_stream.rs
+++ backend/remote_recorder/src/features/monitor_stream.rs
@@ -1,12 +1,13 @@
+use std::time::Duration;
+use stream_throttle::{ThrottlePool, ThrottleRate, ThrottledStream};
+
 use crate::features::events::WriteEvent;
 use crate::utils::writing::NumberWrite;
 use anyhow::Context;
 use flex_net_core::networking::connections::NetConnection;
 use futures::StreamExt;
-use futures::stream::select_all;
-use xcap::Monitor;
-
-use crate::utils::stream::IntoStream;
+use futures::stream::{repeat_with, select_all};
+use xcap::{Frame, Monitor};
 
 use super::events::Event;
 use super::protocol_traits::StreamMonitorFlow;
@@ -17,29 +18,32 @@ impl<T: NetConnection + Send + 'static> StreamMonitorFlow for T {
 
         let monitors = Monitor::all().with_context(|| "failed to get monitors")?;
         let mut streams = Vec::with_capacity(monitors.len());
-        let mut recorders = Vec::with_capacity(monitors.len());
 
         for monitor in monitors {
-            let (recorder, sx) = monitor
-                .video_recorder()
-                .with_context(|| format!("failed to record {:?}", monitor.name()))?;
-
             let id = monitor.id().with_context(|| "failed to get monitor id")?;
-            let stream = sx.into_stream().map(move |f| (f, id));
-
-            recorder
-                .start()
-                .with_context(|| format!("failed to record {:?}", monitor.name()))?;
-
             log::trace!("monitor: {0} {1} prepared", monitor.id()?, monitor.name()?);
 
+            let func = move || {
+                monitor.capture_image().map(|x| {
+                    let width = x.width();
+                    let height = x.height();
+                    let raw = x.into_raw();
+
+                    (Frame::new(width, height, raw), id)
+                })
+            };
+            let stream = repeat_with(func);
+
             streams.push(stream);
-            recorders.push(recorder);
         }
 
         let loop_fn = async move || {
-            let mut multiplexer = select_all(streams);
-            while let Some((frame, id)) = multiplexer.next().await {
+            let rate = ThrottleRate::new(5, Duration::from_millis(16));
+            let pool = ThrottlePool::new(rate);
+
+            let mut multiplexer = select_all(streams).throttle(pool);
+
+            while let Some(Ok((frame, id))) = multiplexer.next().await {
                 self.write_event(Event::Screen).await?;
                 self.write_number(id).await?;
                 self.write_number(frame.width).await?;
@@ -47,22 +51,16 @@ impl<T: NetConnection + Send + 'static> StreamMonitorFlow for T {
 
                 let frame_len: u32 = frame.raw.len().try_into()?;
                 self.write_number(frame_len).await?;
+
                 self.write(frame.raw.as_slice()).await?;
 
                 log::trace!("data send");
-                // why lag?
-                tokio::time::sleep(std::time::Duration::from_secs(3)).await;
-                // break;
             }
 
             Result::<(), anyhow::Error>::Ok(())
         };
 
-        let loop_result = loop_fn().await;
-        for rec in recorders {
-            rec.stop().with_context(|| "failed to stop recorder")?;
-        }
-        loop_result?;
+        loop_fn().await?;
 
         Ok(())
     }
